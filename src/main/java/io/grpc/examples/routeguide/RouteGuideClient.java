ec6ad8b97885b5b3ca51f1097c4e5d4e60c10eca
Jemmy Zhang <hzdan360@163.com>
2018-10-17 18:52:52 +0800

    update grpc_samples
    Signed-off-by: Jemmy Zhang <hzdan360@163.com>

---
+++ src/main/java/io/grpc/examples/routeguide/RouteGuideClient.java
@@ -0,0 +1,159 @@
+package io.grpc.examples.routeguide;
+
+import com.google.common.util.concurrent.SettableFuture;
+import io.grpc.ManagedChannel;
+import io.grpc.StatusRuntimeException;
+import io.grpc.netty.NegotiationType;
+import io.grpc.netty.NettyChannelBuilder;
+import io.grpc.stub.StreamObserver;
+
+import java.util.Iterator;
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author Jemmy Zhang on 2018/10/17.
+ */
+public class RouteGuideClient {
+    private final ManagedChannel channel;
+    private final RouteGuideGrpc.RouteGuideStub asyncStub;
+    private final RouteGuideGrpc.RouteGuideBlockingStub blockingStub;
+    private final RouteGuideGrpc.RouteGuideFutureStub featureStub;
+
+    public RouteGuideClient(String host, int port) {
+        this.channel = NettyChannelBuilder.forAddress(host, port)
+                .negotiationType(NegotiationType.PLAINTEXT)
+                .build();
+        this.asyncStub = RouteGuideGrpc.newStub(channel);
+        this.blockingStub = RouteGuideGrpc.newBlockingStub(channel);
+        this.featureStub = RouteGuideGrpc.newFutureStub(channel);
+    }
+
+    /**
+     * 关闭信道
+     *
+     * @throws InterruptedException
+     */
+    public void shutdown() throws InterruptedException {
+        channel.shutdown().awaitTermination(5, TimeUnit.SECONDS);
+    }
+
+
+    public void blockSend() {
+        Point point = Point.newBuilder().setLatitude(123).setLongitude(234).build();
+        Feature response;
+        try {
+            response = blockingStub.getFeature(point);
+        } catch (StatusRuntimeException e) {
+            System.out.println("RPC调用失败:" + e.getMessage());
+            return;
+        }
+        System.out.println("服务器返回信息:\n" + response.getName() + response.getLocation());
+    }
+
+    public void serverStreamSend() {
+        Rectangle request =
+                Rectangle.newBuilder()
+                        .setLo(Point.newBuilder().setLatitude(11).setLongitude(22).build())
+                        .setHi(Point.newBuilder().setLatitude(33).setLongitude(44).build())
+                        .build();
+        Iterator<Feature> features = blockingStub.listFeatures(request);
+        while (features.hasNext()) {
+            Feature item = features.next();
+            System.out.println(item.getName() + "\n" + item.getLocation());
+        }
+    }
+
+    public void clientStreamSend() throws Exception {
+        final SettableFuture<Void> finishFuture = SettableFuture.create();
+        StreamObserver<RouteSummary> responseObserver = new StreamObserver<RouteSummary>() {
+            @Override
+            public void onNext(RouteSummary summary) {
+                String format = String.format("Finished trip with %d points. Passed %d features. "
+                                + "Travelled %d meters. It took %d seconds.", summary.getPointCount(),
+                        summary.getFeatureCount(), summary.getDistance(), summary.getElapsedTime());
+                System.out.println(format);
+            }
+
+            @Override
+            public void onError(Throwable throwable) {
+                finishFuture.setException(throwable);
+            }
+
+            @Override
+            public void onCompleted() {
+                finishFuture.set(null);
+            }
+        };
+
+        StreamObserver<Point> requestObserver = asyncStub.recordRoute(responseObserver);
+
+        try {
+            Random random = new Random(1000L);
+            Point p1 = Point.newBuilder().setLatitude(random.nextInt()).setLongitude(random.nextInt()).build();
+            Point p2 = Point.newBuilder().setLatitude(random.nextInt()).setLongitude(random.nextInt()).build();
+            requestObserver.onNext(p1);
+            if (finishFuture.isDone()) {
+                return;
+            }
+            Thread.sleep(2000L);
+            requestObserver.onNext(p2);
+            requestObserver.onCompleted();
+            finishFuture.get();
+        } catch (Exception e) {
+            requestObserver.onError(e);
+            throw e;
+        }
+
+    }
+
+    public void dualStreamSend() throws Exception {
+        final SettableFuture<Void> finishFuture = SettableFuture.create();
+        StreamObserver<RouteNote> responseObserver = new StreamObserver<RouteNote>() {
+            @Override
+            public void onNext(RouteNote routeNote) {
+                System.out.println("Receive:" + routeNote.getMessage() +"\n" + routeNote.getLocation());
+            }
+
+            @Override
+            public void onError(Throwable throwable) {
+                finishFuture.setException(throwable);
+            }
+
+            @Override
+            public void onCompleted() {
+                finishFuture.set(null);
+            }
+        };
+
+        StreamObserver<RouteNote> requestObserver = asyncStub.routeChat(responseObserver);
+
+        try {
+            Random random = new Random(1000L);
+            for (int i = 0; i < 10; i++) {
+                Point location = Point.newBuilder().setLongitude(random.nextInt()).setLatitude(random.nextInt()).build();
+                RouteNote routeNote = RouteNote.newBuilder().setLocation(location).setMessage(String.valueOf(i)).build();
+                requestObserver.onNext(routeNote);
+                if (finishFuture.isDone()) {
+                    break;
+                }
+            }
+            requestObserver.onCompleted();
+            finishFuture.get();
+            System.out.println("finish route chat!");
+        } catch (Exception e) {
+            requestObserver.onError(e);
+            throw e;
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        RouteGuideClient client = new RouteGuideClient("127.0.0.1", 50052);
+//        client.blockSend();
+//        client.serverStreamSend();
+//        client.clientStreamSend();
+        client.dualStreamSend();
+        client.shutdown();
+    }
+
+}
---
+++ src/main/java/io/grpc/examples/routeguide/RouteGuideServer.java
@@ -0,0 +1,157 @@
+package io.grpc.examples.routeguide;
+
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+import io.grpc.stub.StreamObserver;
+
+import java.io.IOException;
+import java.util.Objects;
+
+import static java.lang.Math.abs;
+
+/**
+ * @author Jemmy Zhang on 2018/10/17.
+ */
+public class RouteGuideServer {
+
+    private int port = 50052;
+    private Server server;
+
+    private void start() throws IOException {
+        server = ServerBuilder.forPort(port).addService(new RouteGuideServer.RouteGuideService()).build().start();
+        System.out.println("service start...");
+
+        Runtime.getRuntime().addShutdownHook(new Thread() {
+            @Override
+            public void run() {
+                System.out.println("*** shutting down gRPC server since JVM is shutting down.***");
+                RouteGuideServer.this.stop();
+                System.out.println("*** server shutdown.");
+            }
+        });
+
+    }
+
+    private void stop() {
+        if (Objects.nonNull(server)) {
+            server.shutdown();
+        }
+    }
+
+    private void blockUntilShutdown() throws InterruptedException {
+        if (server != null) {
+            server.awaitTermination();
+        }
+    }
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        final RouteGuideServer server = new RouteGuideServer();
+        server.start();
+        server.blockUntilShutdown();
+    }
+
+
+    private static class RouteGuideService extends RouteGuideGrpc.RouteGuideImplBase {
+
+        /**
+         * 简单RPC
+         *
+         * @param request
+         * @param responseObserver
+         */
+        @Override
+        public void getFeature(Point request, StreamObserver<Feature> responseObserver) {
+            Feature feature = checkFeature(request);
+            responseObserver.onNext(feature);
+            responseObserver.onCompleted();
+        }
+
+        private Feature checkFeature(Point location) {
+            System.out.println("构造了新的Feature元素。");
+            return Feature.newBuilder().setName("").setLocation(location).build();
+        }
+
+        /**
+         * 服务端流式RPC
+         *
+         * @param request
+         * @param responseObserver
+         */
+        @Override
+        public void listFeatures(Rectangle request, StreamObserver<Feature> responseObserver) {
+            Point lo = Point.newBuilder(request.getLo()).build();
+            Point hi = Point.newBuilder(request.getHi()).build();
+            responseObserver.onNext(Feature.newBuilder().setName("lo").setLocation(lo).build());
+            responseObserver.onNext(Feature.newBuilder().setName("hi").setLocation(hi).build());
+            responseObserver.onCompleted();
+        }
+
+        /**
+         * 客户端流式RPC
+         *
+         * @param responseObserver
+         * @return
+         */
+        @Override
+        public StreamObserver<Point> recordRoute(StreamObserver<RouteSummary> responseObserver) {
+            return new StreamObserver<Point>() {
+                int pointCount;
+                int featureCount;
+                int distance;
+                Point previous;
+                long startTime = System.nanoTime();
+
+                @Override
+                public void onNext(Point point) {
+                    pointCount++;
+                    if (point.getLatitude() % 2 == 0)
+                        featureCount++;
+                    if (Objects.nonNull(previous)) {
+                        distance = abs((point.getLatitude() - previous.getLatitude()) + (point.getLongitude() - previous.getLongitude()));
+                    }
+                    previous = point;
+                }
+
+                @Override
+                public void onError(Throwable throwable) {
+                    System.out.println("Error occurs: =============>" + throwable.getMessage());
+                }
+
+                @Override
+                public void onCompleted() {
+                    int seconds = (int) ((System.nanoTime() - startTime) / 1000);
+                    responseObserver
+                            .onNext(RouteSummary.newBuilder()
+                                    .setPointCount(pointCount)
+                                    .setFeatureCount(featureCount)
+                                    .setElapsedTime(seconds)
+                                    .setDistance(distance).build());
+                    responseObserver.onCompleted();
+                }
+            };
+        }
+
+
+        @Override
+        public StreamObserver<RouteNote> routeChat(StreamObserver<RouteNote> responseObserver) {
+            return new StreamObserver<RouteNote>() {
+                @Override
+                public void onNext(RouteNote routeNote) {
+                    System.out.println("收到来自客户端的消息：" + routeNote);
+                    responseObserver.onNext(routeNote);
+                }
+
+                @Override
+                public void onError(Throwable throwable) {
+                    System.out.println("Error occurs: =============>" + throwable.getMessage());
+                    responseObserver.onError(throwable);
+                }
+
+                @Override
+                public void onCompleted() {
+                    responseObserver.onCompleted();
+                }
+            };
+        }
+    }
+}
