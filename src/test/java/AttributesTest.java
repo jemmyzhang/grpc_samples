37e8da19f47e0f909d976f36347f8aa359e647d4
Jemmy Zhang <hzdan360@163.com>
2018-10-23 23:22:07 +0800

    ci code
    Signed-off-by: Jemmy Zhang <hzdan360@163.com>

---
+++ src/test/java/AttributesTest.java
@@ -0,0 +1,12 @@
+import io.grpc.Attributes;
+
+/**
+ * Created by JemmyZhang on 2018/10/22
+ */
+public class AttributesTest {
+    public static void main(String[] args) {
+        Attributes attributes=Attributes
+                .newBuilder()
+                .build();
+    }
+}
